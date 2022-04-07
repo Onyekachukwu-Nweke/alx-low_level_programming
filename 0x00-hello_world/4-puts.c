115f58cd625d30e82db739a378f38254962310c9
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-07 14:36:17 +0000

    Added task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *This Program Outputs the word: \"Programming is like building a multilingual puzzle
+ */
+
+int main(void) 
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
