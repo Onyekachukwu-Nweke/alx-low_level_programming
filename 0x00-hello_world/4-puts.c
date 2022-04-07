c00920dc9edb347cb45cf65739cc7d8203739ade
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-07 15:35:01 +0000

    Modified tasks 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - this contains the main block
- * @void : takes no value
- *
- * Return: Always 0
+ *main - main block
+ *Return: 0
  */
-
-int main(void) 
-{	
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
