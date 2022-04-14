9be83e8b0406eda479ed31df9cb626aeb71a5a04
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-14 21:29:39 +0000

    added task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines
+ * @n: int type dat states how long the
+ *     line will be
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,4 @@
 
 This folder contains files on more nested loops
+
+Started Unit testing of C programming tasks from task 6
