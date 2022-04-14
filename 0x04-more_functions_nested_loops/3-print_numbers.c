69105d4365ee4b7ae3ef9d18b832084b02f6439e
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-14 11:49:34 +0000

    Added task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9
+ */
+
+void print_numbers(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
