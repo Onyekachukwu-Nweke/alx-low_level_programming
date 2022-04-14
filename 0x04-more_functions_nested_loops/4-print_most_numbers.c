ee95d0324906c21fba0b0ba92143eb449965ac93
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-14 12:04:26 +0000

    Added task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 - 9 except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		if (c != 2 || c != 4)
+		{
+			_putchar('0' + c);
+		}
+		c++;
+	}
+
+	_putchar('\n');
+}
