f3844bcd32e32d19eb7a52abc8c2f1829cf97625
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-20 22:18:40 +0000

    Added task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,13 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] = '\0')
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
-		i++
+		i++;
 	}
 	_putchar('\n');
 }
