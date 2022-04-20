688b7f06934094c1298d204e24c331c207a3b6ec
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-20 22:34:20 +0000

    Added task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the other half string
+ * @str: string pointer variable
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+
+	for (i /= 2; str != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
