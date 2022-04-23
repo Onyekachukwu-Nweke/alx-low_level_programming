7709d01679ae93f96423a478350c957f1578332a
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-23 09:27:30 +0000

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *string_toupper - converts lowercase to uppercase
+ * @str: string pointer variable
+ * Return: pointer string variable
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
