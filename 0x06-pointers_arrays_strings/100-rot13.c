23b162f46054f566452c41dabafbc96b1603e11b
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-23 18:11:15 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using ROT13
+ * @str: string pointer variable
+ * Return: string pointer
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot[j];
+			}
+		}
+	}
+	return (str);
+}
