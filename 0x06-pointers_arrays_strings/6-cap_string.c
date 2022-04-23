26abfb55f549864df96e1b6326c4d8a40b76f695
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-23 15:41:41 +0000

    Added task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,10 +8,10 @@
 
 char *cap_string(char *str)
 {
-	int i,j;
+	int i, j;
 
 	char c[] = {44, 50, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
-	
+
 	i = 0;
 	while (str[i] != '\0')
 	{
@@ -19,18 +19,16 @@ char *cap_string(char *str)
 		{
 			str[i] = str[i] - 32;
 		}
-
 		j = 0;
 		while (c[j] != '\0')
 		{
-			if (c[j] == str[i] && (str[i+1] >= 97 && str[i+1] <= 122))
+			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
 			{
-				str[i+1] = str[i+1] - 32;
+				str[i + 1] = str[i + 1] - 32;
 			}
 			j++;
 		}
 		i++;
 	}
-
 	return (str);
 }
