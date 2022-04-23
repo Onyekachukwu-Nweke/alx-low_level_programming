a25e3dba681e00220ec70dcf7fc173585658bc88
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-23 08:05:14 +0000

    Modified task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,17 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len, i;
 
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-		;
+	dest_len = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[dest_len + i] = src[i];
-
-	dest[dest_len] = '\0';
+	}
 
 	return (dest);
 }
