be32eb8bfb805110cea0fd809b695483498b65fc
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-26 16:23:41 +0000

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,13 @@
 #include "main .h"
 
 /**
- * _strspn - return the number of occurent of a string
+ * *_strspn - return the number of occurent of a string
  * @s: string to check
  * @accept: character to match
  * Return: int
  */
 
-unsigned int _strspn(char *s, char *accept)
+unsigned int *_strspn(char *s, char *accept)
 {
 	int i, j, cmp = 0;
 
