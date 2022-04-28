54d1370a1ab3e9c15c3f350b3a5b375fa74c4108
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-04-28 16:03:38 +0000

    Task 3

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raise x to power y
+ * @x: first integer
+ * @y: second integer
+ * Return: Alwayys 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else if (y % 2 == 0)
+		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
+	else
+		return (x * _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
+}
