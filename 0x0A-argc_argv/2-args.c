f560033c4c5d8b3dfe91a86f9b925e05716bf650
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-02 20:28:18 +0000

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - contains the main function
+ * @argc: integer variable
+ * @argv: list
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
