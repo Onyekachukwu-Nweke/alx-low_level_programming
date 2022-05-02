353165aec56e2a4df99ca87b44e7ced9cc2c2551
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-02 20:12:05 +0000

    Added task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - contains the main code
+ * @argc: int number
+ * @argv: list
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
