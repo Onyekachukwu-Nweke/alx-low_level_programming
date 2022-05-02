f579ea09181b17c1d8e62a4aeb1f506b19f6b0ae
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-02 20:01:41 +0000

    Added task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,9 @@
  * Return: Always 0.
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char const *argv[])
 {
-	printf("%s \n", argv[0]);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
