72134b55a14176f719fb1f9181d0b3ebfa2f722b
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-02 19:55:28 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - contains the functional block of code
+ * @argc: contains number of arguments
+ * @argv: contains arrays of value of arguments
+ * Return: Always 0.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s \n", argv[0]);
+	return (0);
+}
