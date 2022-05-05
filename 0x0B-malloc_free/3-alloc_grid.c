6176def60abe5a1b44bc7fc3aa8efa9f6daaeba6
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-05 22:48:21 +0000

    Task 3 modified

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 
 	tab = malloc(height * sizeof(*tab));
 
-	if (width <= 0 || height <= 0)
+	if (width <= 0 || height <= 0 || tab == 0)
 	{
 		return (NULL);
 	}
