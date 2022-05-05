4a8883470691f1eec9d5b25a4a1d821903a35820
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-05 18:36:04 +0000

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,9 +30,9 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
