5fd6f98ab7279cdeb632e4e72f6fc1eabae84eb1
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-05 23:25:07 +0000

    Adv. task 2

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -35,33 +35,33 @@ char **strtow(char *str)
 	for (c = height = 0; str[c] != '\0'; c++)
 		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			height++;
-		aout = malloc((height + 1) * sizeof(char *));
-		if (aout == NULL || height == 0)
+	aout = malloc((height + 1) * sizeof(char *));
+	if (aout == NULL || height == 0)
+	{
+		free(aout);
+		return (NULL);
+	}
+	for (i = a1 = 0; i < height; i++)
+	{
+		for (c = a1; str[c] != '\0'; c++)
 		{
-			free(aout);
-			return (NULL);
-		}
-		for (i = a1 = 0; i < height; i++)
-		{
-			for (c = a1; str[c] != '\0'; c++)
+			if (str[c] == ' ')
+				a1++;
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
-				if (str[c] == ' ')
-					a1++;
-				if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+				aout[i] = malloc((c - a1 + 2) * sizeof(char));
+				if (aout[i] == NULL)
 				{
-					aout[i] = malloc((c - a1 + 2) * sizeof(char));
-					if (aout[i] == NULL)
-					{
-						ch_free_grid(aout, i);
-						return (NULL);
-					}
-					break;
+					ch_free_grid(aout, i);
+					return (NULL);
 				}
+				break;
 			}
-			for (j = 0; a1 <= c; a1++, j++)
-				aout[i][j] = str[a1];
-			aout[i][j] = '\0';
 		}
+		for (j = 0; a1 <= c; a1++, j++)
+			aout[i][j] = str[a1];
+		aout[i][j] = '\0';
+	}
 	aout[i] = NULL;
 	return (aout);
 }
