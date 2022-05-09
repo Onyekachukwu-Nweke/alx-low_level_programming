bb66f8d1c2a8aeb6d22df7970a54c099ac8c283a
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-09 13:20:13 +0000

    Modified task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -51,7 +51,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		m[i] = s1[i];
 
-	for (j = 0; s2[j] != '\0' && j <= n; i++, j++)
+	for (j = 0; s2[j] != '\0' && j < n; i++, j++)
 		m[i] = s2[j];
 
 	m[i] = '\0';
