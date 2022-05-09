2659f3779f20a888c6d6efb43a6a5b450e5597fa
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-09 14:52:32 +0000

    Added task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 /**
-* *_memset - fills memory with a constant byte.
-* @s: pointer to put the constant
-* @b: constant
-* @n: max bytes to use
-* Return: s* *_memset - fills memory with a constant byte.
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s* *_memset - fills memory with a constant byte.
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -41,5 +41,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	_memset(m, 0, nmemb * size);
 
-	return (0);
+	return (m);
 }
