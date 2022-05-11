424538d731eae3111e73a147182e1ba20cc52d06
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-11 19:58:31 +0000

    Modified task 2

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	    struct dog my_dog;
 
-	        my_dog.name = "Poppy";
+	       /* my_dog.name = "Poppy";*/
 		    my_dog.age = 3.5;
-		        my_dog.owner = "Bob";
+		       /* my_dog.owner = "Bob"; */
 			    print_dog(&my_dog);
 			        return (0);
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * print-dog - function that prints a struct dog
+ * print_dog - function that prints a struct dog
  * @d - structure pointer variable
  */
 
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name != 0 ? d->name : "(nill)");
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nill)");
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner != 0 ? d->owner : "(nill)");
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nill)");
 	}
 }
