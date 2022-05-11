2dee2ed4d18294824b2ef66dec7f719f0156919d
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-11 20:54:49 +0000

    Added task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,8 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 void free_dog(dog_t *d);
 
 #endif
