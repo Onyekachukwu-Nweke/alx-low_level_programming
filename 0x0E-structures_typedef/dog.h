6debb2894b21c203cb767fcc371f605c42e8bd7a
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-11 15:36:23 +0000

    Added README and Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+
+
+typedef struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+} dog_t;
+
+#endif
