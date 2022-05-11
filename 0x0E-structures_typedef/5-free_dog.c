6ddadb55d53974c758cf516433049bd55f3d3135
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-11 20:32:09 +0000

    added task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free-dog - function that frees dogs
+ * @d: dog struct
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
