9458d9c6358dc5b805afa0c4a00f15d138bb7d22
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-13 19:19:57 +0000

    Added task2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer.
+ * @size: size of the array
+ * @cmp: function pointer
+ * Return: 0 or -1.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	if (array != NULL && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+
+	return (-1);
+}
