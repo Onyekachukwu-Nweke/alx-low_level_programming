fa5a4051b91df42ad820ab003f78fd9a99bef3f9
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-13 13:30:55 +0000

    Added task1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ * @name: string variable
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
