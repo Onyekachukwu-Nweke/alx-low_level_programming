98f8aa37ca8fabea9768c8b7cfdff0c01755adaa
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-14 12:21:55 +0000

    Me

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@
  * @action: function pointer
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, unsigned int size, void (*action)(int))
 {
 	size_t i;
 
