63f76f804e90cbcbc9810b16938fc5fe1e533096
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-16 13:16:21 +0000

    Added task 0 and README.md

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,17 +9,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list sp;
-	unsigned int i, sum;
+	unsigned int i, sum = 0;
 
 	va_start(sp, n);
 
-	if (n == 0)
-		return (0);
-	sum = 0;
-
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(sp, const unsigned int);
+	if (n != 0){
+		for (i = 0; i < n; i++)
+			sum += va_arg(sp, const unsigned int);
 	}
 
 	va_end(sp);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,3 @@
-#include <stdarg.h>
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
