fffc165068cefe6369d987fa3ad09dbc1ccdcdaa
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-05-30 11:24:02 +0000

    Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - function that calculates the length
+ * of a string.
+ * @s: string pointer
+ * Return: length of string.
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
+
+
+/**
+ * binary_to_uint - function that converts a binary
+ * number to an unsigned int
+ * @b: string pointer
+ * Return: unsigned int or 0.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	int i, len;
+
+	if (b == NULL)
+		return (0);
+
+	len = _strlen(b);
+
+	for (i = 0; i != len; i++)
+	{
+		if (b[len - i - 1] == '1')
+			n += 1 << i;
+		else if (b[len - i - 1] != '0')
+			return (0);
+	}
+
+	return (n);
+}
