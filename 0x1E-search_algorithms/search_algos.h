dd214e6f7e110cceeee1a3e79f676a6be23a85cf
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2023-01-13 11:51:57 +0100

    Hotfix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 
 /**
- *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
@@ -46,6 +45,5 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-
 
 #endif /* SEARCH_ALGO_H */
